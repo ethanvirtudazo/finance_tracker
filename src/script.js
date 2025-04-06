2576efbbb382744701508ca6cc8599584821abdc
ethanvirtudazo <ethanjavirtudazo@gmail.com>
2025-04-06 11:25:19 +0900

    Set-Up the Starting Grid

---
+++ src/script.js
@@ -0,0 +1,14 @@
+// selects the table element
+const table = document.getElementById('spreadsheet');
+    
+for (let i = 0; i < 50; i++) {
+    const newRow = document.createElement('tr');
+    table.appendChild(newRow)
+
+    for (let j = 0; j < 50; j++){
+        const newCell = document.createElement('td');
+        newCell.textContent = "";
+        newCell.setAttribute('contenteditable','true')
+        newRow.appendChild(newCell);
+    }            
+}
