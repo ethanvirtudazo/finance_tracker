3b5b36939d9b32d1643e4f8bafc7169d5a9504b4
ethanvirtudazo <ethanjavirtudazo@gmail.com>
2025-04-06 13:18:48 +0900

    Use LocalStorage

--- src/script.js
+++ src/script.js
@@ -1,3 +1,14 @@
+const spreadsheetData = new Map();
+
+const savedData = localStorage.getItem('spreadsheetData');
+
+if (savedData) {
+    const entries = JSON.parse(savedData); // turns it into an array
+    for (const [key, value] of entries) {
+      spreadsheetData.set(key, value);
+    }
+  }
+
 function getColumnLabel(index) {
     let label = '';
     while (index >= 0) {
@@ -6,6 +17,8 @@ function getColumnLabel(index) {
     }
     return label;
 }
+
+
 
 const table = document.getElementById('spreadsheet');
     
@@ -24,12 +37,18 @@ for (let i = 0; i < 50; i++) {
             newCell.textContent = "";
             newCell.setAttribute('contenteditable','false')
         } else if (j === 0 && i > 0) {            
-            newCell.textContent = i        
-        } else {
-            newCell.textContent = "";
-            newCell.dataset.row = i
-            newCell.dataset.col = alphabet
-            newCell.setAttribute('contenteditable','true')
+            newCell.textContent = i;       
+        } else {                        
+            newCell.dataset.col = alphabet;
+            newCell.dataset.row = i;            
+            newCell.setAttribute('contenteditable','true');            
+
+            const cellID = `${alphabet}${i}`;            
+            if (!spreadsheetData.has(cellID)) {
+            spreadsheetData.set(cellID, '');            
+            }
+            
+            newCell.textContent = spreadsheetData.get(cellID);
         }
         newRow.appendChild(newCell);
     }            
@@ -38,6 +57,18 @@ for (let i = 0; i < 50; i++) {
 table.addEventListener("click" , (event) => {
     const cell = event.target;
     const row = cell.dataset.row
-    const col = cell.dataset.col
-    console.log(`you clicked cell ${col}${row}`);
+    const col = cell.dataset.col    
+    cellID = `${cell.dataset.col}${cell.dataset.row}` 
+    spreadsheetData.set(cellID,cell.textContent)
+    console.log(`cell: ${cellID} value: ${spreadsheetData.get(cellID)}`);
+
+})
+
+
+table.addEventListener("input", (event) => {
+    cell = event.target;    
+    cellID = `${cell.dataset.col}${cell.dataset.row}` 
+    spreadsheetData.set(cellID,cell.textContent)  
+
+    localStorage.setItem("spreadsheetData", JSON.stringify([...spreadsheetData]));    
 })
