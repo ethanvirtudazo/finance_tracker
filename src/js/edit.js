940123800591eed9c3d557c8a4e3b7d58539fb30
ethanvirtudazo <ethanjavirtudazo@gmail.com>
2025-04-19 11:46:07 +0900

    Cleaned up Formula Validaiton

--- src/js/edit.js
+++ src/js/edit.js
@@ -1,20 +1,21 @@
 import { ifFormula } from "./formulas.js";
 import { spreadsheetData, table } from "./data.js";
 
-export function addEventListeners() {    
+function getCellID(event) {
+    const cell = event.target;
+    const cellID = `${cell.dataset.col}${cell.dataset.row}`
+    return {cell, cellID}
+}
 
-    table.addEventListener("click" , (event) => {
-        const cell = event.target;
-        const row = cell.dataset.row
-        const col = cell.dataset.col
-        const cellID = `${col}${row}`
+export function addEventListeners() {    
+    table.addEventListener("click" , (event) => {        
+        const {cell, cellID} = getCellID(event)
         spreadsheetData.set(cellID,cell.textContent)
         ifFormula(cell);
     })
     
     table.addEventListener("input", (event) => {
-        const cell = event.target;
-        const cellID = `${cell.dataset.col}${cell.dataset.row}`
+        const {cell, cellID} = getCellID(event)
         spreadsheetData.set(cellID,cell.textContent)    
         localStorage.setItem("spreadsheetData", JSON.stringify([...spreadsheetData]));      
         ifFormula(cell);
--- src/js/formulas.js
+++ src/js/formulas.js
@@ -1,21 +1,31 @@
 import { spreadsheetData, table } from "./data.js"
 
+
+
+function validateFormula(data) {
+    const regex = /^=SUM\(([A-Z]\d+),([A-Z]\d+)\)$/;
+    if (regex.test(data)) {
+        console.log("validated")
+    } else {
+        console.log("invalid")    
+    }              
+}
+
 function trackFormula(cell) {
-    cell.dataset.formula = "formula1"
+    cell.dataset.formula = cell.textContent
 }
 
 export function ifFormula(cell){
     const data = cell.textContent
-    if (data.startsWith("=")) {
-        console.log("Formula")
-        trackFormula(cell)  
-        console.log(cell.dataset.formula)        
-    } else {
-        console.log("Not a formula")
+    if (!data.startsWith("=")) {
+        return        
     }        
+    if (!validateFormula(data)) {
+        return
+    }
+
+    trackFormula(cell)  
 }
-
-
 
 
 export function Formulas() {  
